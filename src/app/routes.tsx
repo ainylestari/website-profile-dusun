2cbfc8c36a11044aa8c9a065e1543afb1c0c9a9f
Ainylestari <ainilestari206@gmail.com>
2026-07-11 20:07:27 +0700

    fixed admin page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -37,7 +37,6 @@ export const router = createBrowserRouter([
     children: [
       { index: true, Component: AdminLogin },
       {
-        path: '/',
         element: (
           <ProtectedRoute>
             <AdminLayout />
